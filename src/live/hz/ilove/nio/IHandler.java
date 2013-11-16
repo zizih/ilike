ad364269f091ea5d3afab3018e02969a26d87e21
zizihjk <zizihjk@gmail.com>
2013-11-16 17:41:22 +0800

    init server

---
+++ src/live/hz/ilove/nio/IHandler.java
@@ -0,0 +1,37 @@
+package live.hz.ilove.nio;
+
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rain
+ * Date: 11/16/13
+ * Time: 4:10 PM
+ * 处理器接口
+ */
+interface IHandler {
+    /**
+     * 处理SelectionKey=OP_ACCEPT事件
+     *
+     * @param key
+     * @throws IOException
+     */
+    void handleAccept(SelectionKey key) throws IOException;
+
+    /**
+     * 处理SelectionKey=OP_READ事件
+     *
+     * @param key
+     * @throws IOException
+     */
+    void handleRead(SelectionKey key) throws IOException;
+
+    /**
+     * 处理SelectionKey=OP_WRITE事件
+     *
+     * @param key
+     * @throws IOException
+     */
+    void handleWrite(SelectionKey key) throws IOException;
+}
---
+++ src/live/hz/ilove/nio/Server.java
@@ -0,0 +1,87 @@
+package live.hz.ilove.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.util.Iterator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rain
+ * Date: 11/16/13
+ * Time: 3:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Server {
+    //通道管理器
+    private Selector selector;
+    private AppHandler handler;
+
+    /**
+     * 获得一个ServerSocket通道，并对该通道做一些初始化的工作
+     *
+     * @param port 绑定的端口号
+     * @throws IOException
+     */
+    public void init(int port) throws IOException {
+        // 获得一个ServerSocket通道
+        ServerSocketChannel serverChannel = ServerSocketChannel.open();
+        // 设置通道为非阻塞
+        serverChannel.configureBlocking(false);
+        // 将该通道对应的ServerSocket绑定到port端口
+        serverChannel.socket().bind(new InetSocketAddress(port));
+        // 获得一个通道管理器
+        this.selector = Selector.open();
+        //将通道管理器和该通道绑定，并为该通道注册SelectionKey.OP_ACCEPT事件,注册该事件后，
+        //当该事件到达时，selector.select()会返回，如果该事件没到达selector.select()会一直阻塞。
+        serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+    }
+
+    /**
+     * 采用轮询的方式监听selector上是否有需要处理的事件，如果有，则进行处理
+     *
+     * @throws IOException
+     */
+    public void listen() throws IOException {
+        System.out.println("服务端启动成功！");
+        // 轮询访问selector
+        while (true) {
+            //当注册的事件到达时，方法返回；否则,该方法会一直阻塞
+            selector.select();
+            // 获得selector中选中的项的迭代器，选中的项为注册的事件
+            Iterator ite = this.selector.selectedKeys().iterator();
+            while (ite.hasNext()) {
+                SelectionKey key = (SelectionKey) ite.next();
+                // 删除已选的key,以防重复处理
+                ite.remove();
+                if (key.isAcceptable()) {
+                    // 客户端请求连接事件
+                    handler.handleAccept(key);
+                } else if (key.isReadable()) {
+                    // 获得了可读的事件
+                    handler.handleRead(key);
+                } else if (key.isWritable()) {
+                    // 获得了可写的事件
+                    handler.handleWrite(key);
+                }
+
+            }
+
+        }
+    }
+
+
+    /**
+     * 启动服务端测试
+     *
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        Server server = new Server();
+        server.init(8000);
+        server.listen();
+    }
+
+}
